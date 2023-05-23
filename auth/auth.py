b02b27dd9965791b2797415a24fea48b98f4349d
vickouma77 <Vickprestone20@gmail.com>
2023-05-23 15:03:53 +0300

    updating templatesand auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -7,6 +7,11 @@ from flask import Blueprint, render_template
 from EcoExchange.models import auth
 
 
-@auth_bp = Blueprint('auth_bp', __name__,
+auth_bp = Blueprint("auth_bp", __name__,
                      template_folder='templates'
                      static_folder='static', static_url_path='assets')
+
+
+@auth_bp.route('/login')
+def login():
+    return render_template('auth/login.html', login=login)
