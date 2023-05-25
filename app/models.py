5d68fe0e93ba8a123c797d45076c319208dd8400
vickouma77 <Vickprestone20@gmail.com>
2023-05-25 20:10:19 +0300

    adding blank line in line 5

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
