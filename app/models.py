c66ab3fc1005ea0379d09e0ecf11d6749b9ee1d3
vickouma77 <Vickprestone20@gmail.com>
2023-05-25 20:08:46 +0300

    creating user model

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
