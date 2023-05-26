b94608d3bd901ceb826a696c10342fa0b49e5ad4
vickouma77 <Vickprestone20@gmail.com>
2023-05-26 17:18:52 +0300

    creating routes to handle user registration and logins

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from .forms import RegistrationForm, LoginForm
+from .models import User
+from . import app, db
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
