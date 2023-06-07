564f1e63afb229114057a05c3f8c2bb1fcb6863b
vickouma77 <Vickprestone20@gmail.com>
2023-06-07 18:44:15 +0300

    initiating data migration

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,15 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(
+    username = StringField('username', validators=[DataRequired(), Length(
                                                   min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    email = StringField('Email', validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm Password', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('email', validators=[DataRequired()])
+    submit = SubmitField('register')
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Log In')
+    submit = SubmitField('login')
